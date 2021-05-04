489fcc136e7b94ca6b0fa3e431729983f76f2d3b
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-05-04 13:15:42 +0530

    complete views

--- server/snippets/urls.py
+++ server/snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets.views import snippet_detail, snippet_list
+
 
 urlpatterns = [
     path('snippets', snippet_list),
     path('snippets/<int:pk>/', snippet_detail)
 ]
+
+format_suffix_patterns(urlpatterns)
